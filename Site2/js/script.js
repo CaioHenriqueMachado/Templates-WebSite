9bc76a7dd764b44cb2703a0f7bf4f8a2eb5a8b61
Caio <caio.student@outlook.com>
2020-07-26 17:41:10 -0300

    ajuste do site 2

--- Site2/js/script.js
+++ Site2/js/script.js
@@ -11,5 +11,24 @@ menuIcon.onclick = function(){
 	else {
 			sideNav.style.right = "-110px"
 	}
-
 }
+
+// FORM CAROUSEL
+let count = 0;
+
+document.querySelector("#items-carousel")
+.addEventListener("wheel", event => {
+  if(event.deltaY > 0) {
+    count += 300;
+    event.target.scrollBy(300,0);
+  }else {
+    count -= 300;
+    event.target.scrollBy(-300,0);
+  }
+  });
+
+setInterval(function(){
+  count += 300
+  if(count > 2400) count = 0
+  document.querySelector("#items-carousel").scrollTo(count, 0);
+}, 5000);
