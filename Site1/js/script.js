98024f76c565837397003c01514d69752a969d37
Caio <caio.student@outlook.com>
2020-07-26 18:51:30 -0300

    identação site 1

--- Site1/js/script.js
+++ Site1/js/script.js
@@ -1,4 +1,12 @@
 // FOR MENU
+$(window).on("scroll", function() {
+  if($(window).scrollTop()) {
+      $('div.topo').addClass('corTopo');
+  } else {
+      $('div.topo').removeClass('corTopo');
+  }  
+})
+
 // FORM CAROUSEL
 let count = 0;
 
@@ -18,3 +26,5 @@ setInterval(function(){
   if(count > 2400) count = 0
   document.querySelector("#items-carousel").scrollTo(count, 0);
 }, 2000);
+
+
